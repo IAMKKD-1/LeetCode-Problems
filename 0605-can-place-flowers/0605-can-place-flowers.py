5c777b5b3ac24721050c2d87c0393b99aadb8d1b
Krishnakant Dubey <88988596+IAMKKD-1@users.noreply.github.com>
2024-08-06 19:32:43 +0530

    Time: 129 ms (71.26%), Space: 16.8 MB (89.03%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,27 +1,37 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        count = 0
-        if len(flowerbed) == 1:
-            if flowerbed[0] == 1:
-                if n == 1:
-                    return False
-                else:
-                    return True
-            else:
-                return True
-                
-        for i in range(0, len(flowerbed)):
-            print(i)
-            if i == 0 and flowerbed[i] + flowerbed[i+1] == 0:
-                count += 1
-                flowerbed[i] = 1  
-            elif i == len(flowerbed) - 1:
-                if flowerbed[i] + flowerbed[i-1] == 0:
-                    count += 1
-                    flowerbed[i] = 1
-            elif flowerbed[i] + flowerbed[i+1] + flowerbed[i-1] == 0:
-                count += 1
+        for i in range(len(flowerbed)):
+            left = i == 0 or flowerbed[i-1] == 0
+            right = i == len(flowerbed) - 1 or flowerbed[i+1] == 0
+            if left and right and flowerbed[i] == 0:
                 flowerbed[i] = 1
-        if count >= n:
+                n -=1
+        if n<=0:
             return True
         return False
+
+        # count = 0
+        # if len(flowerbed) == 1:
+        #     if flowerbed[0] == 1:
+        #         if n == 1:
+        #             return False
+        #         else:
+        #             return True
+        #     else:
+        #         return True
+                
+        # for i in range(0, len(flowerbed)):
+        #     print(i)
+        #     if i == 0 and flowerbed[i] + flowerbed[i+1] == 0:
+        #         count += 1
+        #         flowerbed[i] = 1  
+        #     elif i == len(flowerbed) - 1:
+        #         if flowerbed[i] + flowerbed[i-1] == 0:
+        #             count += 1
+        #             flowerbed[i] = 1
+        #     elif flowerbed[i] + flowerbed[i+1] + flowerbed[i-1] == 0:
+        #         count += 1
+        #         flowerbed[i] = 1
+        # if count >= n:
+        #     return True
+        # return False
