bcfe85e861e33ae9b89efba221fa8bcf01579d38
Krishnakant Dubey <88988596+IAMKKD-1@users.noreply.github.com>
2024-09-30 19:14:05 +0530

    Time: 2109 ms (86.14%), Space: 16.7 MB (33.01%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -4,17 +4,17 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        def backtrack(grid, m, n, row, col, gold):
+        def backtrack(row, col, gold):
             if row < 0 or col < 0 or row >= m or col >= n or grid[row][col] == 0:
                 return gold
 
             curr = grid[row][col]
             grid[row][col] = 0
 
-            up = backtrack(grid, m, n, row-1, col, gold + curr)
-            down = backtrack(grid, m, n, row+1, col, gold + curr)
-            left = backtrack(grid, m, n, row, col-1, gold + curr)
-            right = backtrack(grid, m, n, row, col+1, gold + curr)
+            up = backtrack(row-1, col, gold + curr)
+            down = backtrack(row+1, col, gold + curr)
+            left = backtrack(row, col-1, gold + curr)
+            right = backtrack(row, col+1, gold + curr)
             
             grid[row][col] = curr
 
@@ -25,6 +25,6 @@ class Solution:
                 if grid[i][j] == 0:
                     continue
 
-                max_gold = max(max_gold, backtrack(grid, m, n, i, j, 0))
+                max_gold = max(max_gold, backtrack(i, j, 0))
                 
         return max_gold
