a841994bff0ed862d32f559ce7c7cfe90e8ba9bc
Krishnakant Dubey <88988596+IAMKKD-1@users.noreply.github.com>
2024-12-03 16:25:14 +0530

    Time: 79 ms (95.46%), Space: 17.6 MB (18.55%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -5,10 +5,19 @@ class Solution:
         
         Max_Len=1
         Max_Str=s[0]
-        for i in range(len(s)-1):
-            for j in range(i+1,len(s)):
-                if j-i+1 > Max_Len and s[i:j+1] == s[i:j+1][::-1]:
-                    Max_Len = j-i+1
-                    Max_Str = s[i:j+1]
-
+        s = '#' + '#'.join(s) + '#'
+        dp = [0 for _ in range(len(s))]
+        center = 0
+        right = 0
+        for i in range(len(s)):
+            if i < right:
+                dp[i] = min(right-i, dp[2*center-i])
+            while i-dp[i]-1 >= 0 and i+dp[i]+1 < len(s) and s[i-dp[i]-1] == s[i+dp[i]+1]:
+                dp[i] += 1
+            if i+dp[i] > right:
+                center = i
+                right = i+dp[i]
+            if dp[i] > Max_Len:
+                Max_Len = dp[i]
+                Max_Str = s[i-dp[i]:i+dp[i]+1].replace('#','')
         return Max_Str
