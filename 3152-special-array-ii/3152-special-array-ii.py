de3420e2519ece315d10d8259e049113edb6a4ab
Krishnakant Dubey <88988596+IAMKKD-1@users.noreply.github.com>
2024-12-09 16:09:48 +0530

    Time: 43 ms (65.28%), Space: 46.6 MB (53.04%) - LeetHub

---
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+        n = len(nums)
+        prefix = [0] * n  
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1]
+            if (nums[i - 1] % 2 == 0 and nums[i] % 2 == 0) or (nums[i - 1] % 2 != 0 and nums[i] % 2 != 0):
+                prefix[i] += 1
+
+        result = [] 
+
+        for left, right in queries:
+            special_count = prefix[right] - (prefix[left] if left > 0 else 0)
+            result.append(special_count == 0)
+
+        return result
